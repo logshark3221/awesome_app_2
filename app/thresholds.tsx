d7f5f580d3db7ea036a588035aa6e6e25c22af0a
William <wdcherry03@gmail.com>
2026-02-23 12:16:12 -0600

    Changed to ScrollView to fix keypad covering screen on mobile

--- app/thresholds.tsx
+++ app/thresholds.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
-import { StyleSheet, TextInput } from 'react-native';
+import { KeyboardAvoidingView, Platform, ScrollView, StyleSheet, TextInput } from 'react-native';
 
-import ParallaxScrollView from '@/components/parallax-scroll-view';
 import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Fonts } from '@/constants/theme';
 import { useScreen } from '@/hooks/use-screen';
 
 import AsyncStorage from '@react-native-async-storage/async-storage';
@@ -42,78 +38,68 @@ export default function ThresholdScreen() {
   }
 
   // Reads thresholds
-  readThresholds('H2SThreshold', onChangeH2S)
-  readThresholds('O2Threshold', onChangeO2)
-  readThresholds('COThreshold', onChangeCO)
-  readThresholds('CH4Threshold', onChangeCH4)
-  readThresholds('TemperatureThreshold', onChangeTemperature)
+  React.useEffect(() => {
+    readThresholds('H2SThreshold', onChangeH2S)
+    readThresholds('O2Threshold', onChangeO2)
+    readThresholds('COThreshold', onChangeCO)
+    readThresholds('CH4Threshold', onChangeCH4)
+    readThresholds('TemperatureThreshold', onChangeTemperature)
+  }, []);
   
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#D0D0D0', dark: '#353636' }}
-      headerImage={
-        <IconSymbol
-          size={310}
-          color="#808080"
-          name="chevron.left.forwardslash.chevron.right"
-          style={styles.headerImage}
+    <KeyboardAvoidingView
+      style={{flex: 1 }}
+      behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
+      <ScrollView
+        contentContainerStyle={styles.content}
+        keyboardShouldPersistTaps="handled"
+        showsVerticalScrollIndicator={true}>
+        <ThemedText>H₂S</ThemedText>
+          <TextInput
+            style={styles.input}
+            onChangeText={(text) => storeThresholds(onChangeH2S, 'H2SThreshold', text)}
+            value={H2SThreshold}
+            placeholder="100.00"
+            keyboardType="numeric"
         />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText
-          type="title"
-          style={{
-            fontFamily: Fonts.rounded,
-          }}>
-          Thresholds
-        </ThemedText>
-      </ThemedView>
 
-      <ThemedText>H₂S</ThemedText>
+        <ThemedText>O₂</ThemedText>
         <TextInput
           style={styles.input}
-          onChangeText={(text) => storeThresholds(onChangeH2S, 'H2SThreshold', text)}
-          value={H2SThreshold}
+          onChangeText={(text) => storeThresholds(onChangeO2, 'O2Threshold', text)}
+          value={O2Threshold}
+          placeholder="235000.00"
+          keyboardType="numeric"
+        />
+
+        <ThemedText>CO</ThemedText>
+        <TextInput
+          style={styles.input}
+          onChangeText={(text) => storeThresholds(onChangeCO, 'COThreshold', text)}
+          value={COThreshold}
           placeholder="100.00"
           keyboardType="numeric"
-      />
+        />
 
-      <ThemedText>O₂</ThemedText>
-      <TextInput
-        style={styles.input}
-        onChangeText={(text) => storeThresholds(onChangeO2, 'O2Threshold', text)}
-        value={O2Threshold}
-        placeholder="235000.00"
-        keyboardType="numeric"
-      />
+        <ThemedText>CH₄</ThemedText>
+        <TextInput
+          style={styles.input}
+          onChangeText={(text) => storeThresholds(onChangeCH4, 'CH4Threshold', text)}
+          value={CH4Threshold}
+          placeholder="5000.00"
+          keyboardType="numeric"
+        />
 
-      <ThemedText>CO</ThemedText>
-      <TextInput
-        style={styles.input}
-        onChangeText={(text) => storeThresholds(onChangeCO, 'COThreshold', text)}
-        value={COThreshold}
-        placeholder="100.00"
-        keyboardType="numeric"
-      />
-
-      <ThemedText>CH₄</ThemedText>
-      <TextInput
-        style={styles.input}
-        onChangeText={(text) => storeThresholds(onChangeCH4, 'CH4Threshold', text)}
-        value={CH4Threshold}
-        placeholder="5000.00"
-        keyboardType="numeric"
-      />
-
-      <ThemedText>Temperature</ThemedText>
-      <TextInput
-        style={styles.input}
-        onChangeText={(text) => storeThresholds(onChangeTemperature, 'TemperatureThreshold', text)}
-        value={TemperatureThreshold}
-        placeholder="95.00"
-        keyboardType="numeric"
-      />
-    </ParallaxScrollView>
+        <ThemedText>Temperature</ThemedText>
+        <TextInput
+          style={styles.input}
+          onChangeText={(text) => storeThresholds(onChangeTemperature, 'TemperatureThreshold', text)}
+          value={TemperatureThreshold}
+          placeholder="95.00"
+          keyboardType="numeric"
+        />
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
 
@@ -124,10 +110,18 @@ const createStyles = (windowWidth: number, windowHeight: number) => StyleSheet.c
     left: -35,
     position: 'absolute',
   },
+
+  content: {
+    flexGrow: 1,
+    padding: 20,
+    backgroundColor: '#D3D3D3',
+  },
+
   titleContainer: {
     flexDirection: 'row',
     gap: 8,
   },
+
   input: {
     height: 40,
     margin: 12,
