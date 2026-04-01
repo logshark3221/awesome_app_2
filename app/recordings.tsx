95b216fb467d9c3fba8ff1a65efea41134fcaa15
Wolla98 <winstonbounsavy@gmail.com>
2026-03-31 21:33:26 -0500

    Added basic file systems tests (expo-file-system)

--- app/recordings.tsx
+++ app/recordings.tsx
@@ -1,51 +1,34 @@
 import React from 'react';
-import { KeyboardAvoidingView, Platform, ScrollView, StyleSheet, TextInput } from 'react-native';
+import { Button, KeyboardAvoidingView, Platform, ScrollView, StyleSheet, TextInput } from 'react-native';
 
 import { ThemedText } from '@/components/themed-text';
 import { useScreen } from '@/hooks/use-screen';
 
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import { File, Directory, Paths } from 'expo-file-system';
+
 
 export default function ThresholdScreen() {
   const { windowWidth, windowHeight } = useScreen();
   const styles = createStyles(windowWidth, windowHeight);
 
-  const [H2SThreshold, onChangeH2S] = React.useState('-1');
-  const [O2Threshold, onChangeO2] = React.useState('-1');
-  const [COThreshold, onChangeCO] = React.useState('-1');
-  const [CH4Threshold, onChangeCH4] = React.useState('-1');
-  const [TemperatureThreshold, onChangeTemperature] = React.useState('-1');
-
-  // Testing persistent data storage with async storage
-  const readThresholds = async (key: string, updateFunction: (arg0: string) => void) => {
+  const test = () => {
     try {
-      const value = await AsyncStorage.getItem(key);
-      if (value !== null) {
-        updateFunction(value);
-      }
+      const file = new File(Paths.cache, 'example.txt');
 
-      else {
-        updateFunction('');
+      // Write
+      if (! file.exists) {
+        file.create(); // can throw an error if the file already exists or no permission to create it
+        file.write('Hello, world!');
       }
-    } catch(e) { }
+      
+      // Read
+      console.log(file.textSync()); // Hello, world!
+
+    } catch (error) {
+      console.error(error);
+    }
   }
 
-  const storeThresholds = async (updateFunction: (arg0: string) => void, key:string, value: string) => {
-    try {
-      await AsyncStorage.setItem(key, value);
-      updateFunction(value);
-    } catch(e) { }
-  }
-
-  // Reads thresholds
-  React.useEffect(() => {
-    readThresholds('H2SThreshold', onChangeH2S)
-    readThresholds('O2Threshold', onChangeO2)
-    readThresholds('COThreshold', onChangeCO)
-    readThresholds('CH4Threshold', onChangeCH4)
-    readThresholds('TemperatureThreshold', onChangeTemperature)
-  }, []);
-  
   return (
     <KeyboardAvoidingView
       style={{flex: 1 }}
@@ -54,50 +37,13 @@ export default function ThresholdScreen() {
         contentContainerStyle={styles.content}
         keyboardShouldPersistTaps="handled"
         showsVerticalScrollIndicator={true}>
-        <ThemedText>H₂S</ThemedText>
-          <TextInput
-            style={styles.input}
-            onChangeText={(text) => storeThresholds(onChangeH2S, 'H2SThreshold', text)}
-            value={H2SThreshold}
-            placeholder="10.00"
-            keyboardType="numeric"
-        />
 
-        <ThemedText>O₂</ThemedText>
-        <TextInput
-          style={styles.input}
-          onChangeText={(text) => storeThresholds(onChangeO2, 'O2Threshold', text)}
-          value={O2Threshold}
-          placeholder="23.50"
-          keyboardType="numeric"
+        <Button
+          onPress={test}
+          title={"Test Button"}
+          color='#808080'
         />
-
-        <ThemedText>CO</ThemedText>
-        <TextInput
-          style={styles.input}
-          onChangeText={(text) => storeThresholds(onChangeCO, 'COThreshold', text)}
-          value={COThreshold}
-          placeholder="50.00"
-          keyboardType="numeric"
-        />
-
-        <ThemedText>CH₄</ThemedText>
-        <TextInput
-          style={styles.input}
-          onChangeText={(text) => storeThresholds(onChangeCH4, 'CH4Threshold', text)}
-          value={CH4Threshold}
-          placeholder="5.00"
-          keyboardType="numeric"
-        />
-
-        <ThemedText>Temperature</ThemedText>
-        <TextInput
-          style={styles.input}
-          onChangeText={(text) => storeThresholds(onChangeTemperature, 'TemperatureThreshold', text)}
-          value={TemperatureThreshold}
-          placeholder="95.00"
-          keyboardType="numeric"
-        />
+        
       </ScrollView>
     </KeyboardAvoidingView>
   );
