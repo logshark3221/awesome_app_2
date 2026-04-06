8a5b88bd54b1a91c59912e87cc816dc5ba5739b3
Logan <yoboysharp@gmail.com>
2026-04-06 11:38:33 -0500

    Added USE_BLUETOOTH const in use-bluetooth-data.ts

--- hooks/use-bluetooth-data.ts
+++ hooks/use-bluetooth-data.ts
@@ -1,8 +1,8 @@
 import useBLE from '@/hooks/use-BLE';
 import { useEffect, useState } from "react";
 import { BluetoothContextType } from "./bluetooth-context";
-import { ParsedPacket, parsePacket } from "./packet-parser";
-
+import { generatePacket, ParsedPacket, parsePacket } from "./packet-parser";
+const USE_BLUETOOTH = false; // True for bluetooth, false for emulator/web testing
 
 export function useBluetoothData(): BluetoothContextType {
   const {
@@ -17,7 +17,9 @@ export function useBluetoothData(): BluetoothContextType {
   const [latestPacket, setLatestPacket] = useState<ParsedPacket | null>(null);
   const [history, setHistory] = useState<ParsedPacket[]>([]);
 
+  // Actual BT code
   useEffect(() => {
+    if (!USE_BLUETOOTH) return;
     if (!HazmatReads || HazmatReads === "[]") return;
 
     const parsed = parsePacket(HazmatReads);
@@ -29,7 +31,23 @@ export function useBluetoothData(): BluetoothContextType {
       });
     }
   }, [HazmatReads]);
+  // Packet generation, no BT
+  useEffect(() => {
+    if (USE_BLUETOOTH) return;
+    const interval = setInterval(() => {
+      const raw = generatePacket();
+      const parsed = parsePacket(raw);
+      if (parsed) {
+        setLatestPacket(parsed);
+        setHistory(prev => {
+          const updated = [...prev, parsed];
+          return updated.slice(-50);
+        })
+      }
+    }, 5000); // generate a new packet every 5000ms
 
+    return () => clearInterval(interval);
+  }, []);
   return {
     latestPacket,
     history,
