413b638563d4b1a998b803588f84d1184b2d0569
Wolla98 <winstonbounsavy@gmail.com>
2026-04-07 01:12:26 -0500

    Added prototype for displaying stored readings

--- app/recordings.tsx
+++ app/recordings.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Button, KeyboardAvoidingView, Platform, ScrollView, StyleSheet } from 'react-native';
+import { LineChart } from 'react-native-gifted-charts';
 
 import { useScreen } from '@/hooks/use-screen';
 
@@ -7,69 +8,106 @@ import { ThemedText } from '@/components/themed-text';
 import { File, Paths } from 'expo-file-system';
 
 import { useBluetoothContext } from '@/hooks/bluetooth-context';
+import { ParsedPacket } from '@/hooks/packet-parser';
 
 export default function ThresholdScreen() {
-  const { windowWidth, windowHeight } = useScreen();
-  const styles = createStyles(windowWidth, windowHeight);
 
-  const { history, clearHistory } = useBluetoothContext();
-  const sessionData = history.map(packet => {return packet})
+    // Styles
+    const { windowWidth, windowHeight } = useScreen();
+    const styles = createStyles(windowWidth, windowHeight);
 
-  const writeToFile = () => {
-    try {
+    // Bluetooth signal history data
+    const { history, clearHistory } = useBluetoothContext();
+    const sessionData = history.map(packet => {return packet})
 
-      // Gets file
-      const file = new File(Paths.cache, 'example.json');
-      if (! file.exists) {
-        file.create(); // Can throw an error if the file already exists or no permission to create it
+    // Chart Stuff
+    const chartData = history.map(packet => {return {value : packet.parsedH2S}}).slice(-50)
+
+    // Writes the current session to file
+    const writeToFile = () => {
+      try {
+
+        // Gets file
+        const file = new File(Paths.cache, 'example.json');
+        if (! file.exists) {
+          file.create(); // Can throw an error if the file already exists or no permission to create it
+        }
+
+        // Converts history to a string, and writes it
+        const temp = JSON.stringify(history);
+        file.write(temp);
+
+        console.log("File written successfully")
+
+      } catch (error) {
+        console.error(error);
       }
-
-      // Converts history to a string, and writes it
-      const temp = JSON.stringify(history);
-      file.write(temp);
-
-      console.log("File written successfully")
-
-    } catch (error) {
-      console.error(error);
     }
-  }
+  
+    // Reads the session saved in file and prints it to the log (and stores it in memory)
+    const readFromFile = () => {
+      try {
 
-  const readFromFile = () => {
-    try {
+        // Gets file
+        const file = new File(Paths.cache, 'example.json');
+        if (! file.exists) {
+          console.log("File does not exist!")
+          return
+        }
 
-      // Gets file
-      const file = new File(Paths.cache, 'example.json');
-      if (! file.exists) {
-        console.log("File does not exist!")
-        return
+        const temp = file.textSync();
+        const temp2 = JSON.parse(temp)
+
+        // Prints to log
+        console.log(temp2)
+        console.log(temp2.map((entry: ParsedPacket) => {return {value : entry.parsedH2S}}).slice(-50))
+
+        changeTempData(temp2.map((entry: ParsedPacket) => {return {value : entry.parsedH2S}}).slice(-50))
+        changeShowRecordedChart(true)
+
+      } catch (error) {
+        console.error(error);
       }
-
-      const temp = file.textSync();
-      console.log(JSON.parse(temp))
-
-    } catch (error) {
-      console.error(error);
     }
-  }
 
-  const cleanFiles = () => {
-    try {
+    // Deletes the json and text files
+    const cleanFiles = () => {
+      try {
 
-      const file1 = new File(Paths.cache, 'example.json');
-      const file2 = new File(Paths.cache, 'example.txt');
-      file1.delete()
-      file2.delete()
+        const file1 = new File(Paths.cache, 'example.json');
+        const file2 = new File(Paths.cache, 'example.txt');
+        file1.delete()
+        file2.delete()
 
-      console.log("Files cleaned successfully")
-    } catch (error) {
-      console.error(error);
+        console.log("Files cleaned successfully")
+      } catch (error) {
+        console.error(error);
+      }
     }
-  }
 
-  const printToLog = () => {
-    console.log(sessionData)
-  }
+    // Prints the current session to the console
+    const printToConsole = () => {
+      console.log(sessionData)
+    }
+
+
+    // Some graph stuff for the recorded graph. Strongly consider deleting later
+    const [showRecordedChart, changeShowRecordedChart] = React.useState(false);
+    const [tempData, changeTempData] = React.useState([])
+    function ConditionalGraph( { showChart } : { showChart : boolean}) {
+      if (showChart) {
+        return <LineChart
+            data={tempData}
+            width={windowWidth}
+            height={300}
+            spacing={(windowWidth / chartData.length) / 1.4}
+            hideDataPoints={false}
+            yAxisLabelTexts={[]}
+            yAxisColor="transparent"
+        />
+      }
+      return null
+    }
 
   return (
     <KeyboardAvoidingView
@@ -99,7 +137,7 @@ export default function ThresholdScreen() {
         />
 
         <Button
-          onPress={printToLog}
+          onPress={printToConsole}
           title={"Print Session to Log"}
           color='#808080'
         />
@@ -111,6 +149,31 @@ export default function ThresholdScreen() {
         />
 
         <ThemedText darkColor='black' lightColor='black'>Current Session Size: {sessionData.length}</ThemedText>
+
+        <LineChart
+            data={chartData}
+            width={windowWidth}
+            height={300}
+            spacing={(windowWidth / chartData.length) / 1.4}
+            hideDataPoints={false}
+            yAxisLabelTexts={[]}
+            yAxisColor="transparent"
+        />
+
+        <ThemedText darkColor='black' lightColor='black'>Recorded Session (JSON File) Size: {tempData.length}</ThemedText>
+        <LineChart
+            data={tempData}
+            width={windowWidth}
+            height={300}
+            spacing={(windowWidth / tempData.length) / 1.4}
+            hideDataPoints={false}
+            yAxisLabelTexts={[]}
+            yAxisColor="transparent"
+        />
+
+        {/* <ConditionalGraph
+          showChart = {showRecordedChart}
+        /> */}
         
       </ScrollView>
     </KeyboardAvoidingView>
